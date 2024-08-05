1b81edbc22128adb54fc97e903fb0b526f8d4bba
shuharib0t <shuharib0t@gmail.com>
2024-08-05 18:36:05 -0300

    add coin-chart and initial skeleton-chart

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -1,9 +1,14 @@
 import { CoinCarousel } from '@/components/coin-carousel'
+import { CoinChart } from '@/components/coin-graph'
 
 export default function Home() {
   return (
     <div className="p-20">
-      <h1>hello world</h1>
+      <div className="flex flex-col gap-3">
+        <h1>Selecione</h1>
+
+        <CoinChart />
+      </div>
 
       <div className="flex justify-center">
         <CoinCarousel />
